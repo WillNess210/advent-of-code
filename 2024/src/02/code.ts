986231ad88f89059c5eb13bc30ecd563b2c48970
Will Ness <willness210@gmail.com>
2024-12-02 10:09:04 -0700

    day 2 part 2

--- 2024/src/02/code.ts
+++ 2024/src/02/code.ts
@@ -1,11 +1,8 @@
 import * as fs from "fs";
 
-// code.ts src/01/example.txt
-// get filename to read from command line arguments
 const args = process.argv.slice(2);
 const filename = args[0];
 
-console.log(filename);
 const input = fs.readFileSync(filename, "utf-8");
 
 const parseInput = (input: string): number[][] => {
@@ -13,23 +10,54 @@ const parseInput = (input: string): number[][] => {
 };
 
 const MAX_CHANGE = 3;
-const isReportSafe = (report: number[]): boolean => {
+// returns the index of the first element that is not safe, or "safe" if all elements are safe
+const isReportSafe = (report: number[]): number | "safe" => {
   const direction = report[0] - report[1] > 0 ? -1 : 1;
   for (let i = 1; i < report.length; i++) {
     const difference = report[i] - report[i - 1];
     if (Math.sign(difference) !== direction) {
-      return false;
+      return i;
     } else if (Math.abs(difference) > MAX_CHANGE) {
-      return false;
+      return i;
     }
   }
-  return true;
+  return "safe";
 };
 
 const calculateSafeReports = (reports: number[][]): number => {
-  return reports.filter(isReportSafe).length;
+  return reports.filter((report) => isReportSafe(report) === "safe").length;
+};
+
+const removeIndexAndCheckSafety = (
+  report: number[],
+  indexToRemove: number
+): boolean => {
+  const reportWithoutIndex = report.slice();
+  reportWithoutIndex.splice(indexToRemove, 1);
+  return isReportSafe(reportWithoutIndex) === "safe";
+};
+
+const isReportSafeWithDampeners = (report: number[]): boolean => {
+  const reportSafeResult = isReportSafe(report);
+  if (reportSafeResult === "safe") {
+    return true;
+  }
+  if (removeIndexAndCheckSafety(report, 0)) {
+    return true;
+  }
+  for (let i = 0; i < 2; i++) {
+    if (removeIndexAndCheckSafety(report, reportSafeResult - i)) {
+      return true;
+    }
+  }
+  return false;
+};
+const calculateSafeReportsWithDampeners = (reports: number[][]): number => {
+  return reports.filter(isReportSafeWithDampeners).length;
 };
 
 const reports = parseInput(input);
 const safeReports = calculateSafeReports(reports);
 console.log(`Safe reports: ${safeReports}`);
+const safeReportsWithDampeners = calculateSafeReportsWithDampeners(reports);
+console.log(`Safe reports with dampeners: ${safeReportsWithDampeners}`);
