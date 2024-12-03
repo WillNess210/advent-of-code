65a64712a694fd795d97acc705f3828d6da682eb
Will Ness <willness210@gmail.com>
2024-12-03 04:51:19 -0700

    day 3 part 2

--- 2024/src/03/code.ts
+++ 2024/src/03/code.ts
@@ -4,20 +4,44 @@ const args = process.argv.slice(2);
 const filename = args[0];
 
 const input = fs.readFileSync(filename, "utf-8");
-const lines = input.split("\n");
 
 const isNumeric = (char: string): boolean => /^[0-9]$/.test(char);
 
 const LINE_START = "mul(";
-const processLine = (line: string): number => {
+const ENABLE_STRING = "do()";
+const DISABLE_STRING = "don't()";
+const MOST_RECENT_CHARS_HISTORY = Math.max(
+  ENABLE_STRING.length,
+  DISABLE_STRING.length
+);
+const processInput = (input: string): number => {
+  let enabled = true;
   let sum = 0;
   let current = "";
   let num1 = 0;
   let num2 = 0;
   let stage: "start" | "num1" | "num2" = "start";
+  let mostRecentChars = "";
   // iterate over the line
-  for (let i = 0; i < line.length; i++) {
-    const char = line[i];
+  for (let i = 0; i < input.length; i++) {
+    const char = input[i];
+
+    if (mostRecentChars.length === MOST_RECENT_CHARS_HISTORY) {
+      mostRecentChars = mostRecentChars.slice(1);
+    }
+    mostRecentChars += char;
+    if (mostRecentChars.slice(-ENABLE_STRING.length) === ENABLE_STRING) {
+      enabled = true;
+    } else if (
+      mostRecentChars.slice(-DISABLE_STRING.length) === DISABLE_STRING
+    ) {
+      enabled = false;
+    }
+
+    if (!enabled) {
+      continue;
+    }
+
     if (stage === "start") {
       if (char === LINE_START[current.length]) {
         current += char;
@@ -56,5 +80,5 @@ const processLine = (line: string): number => {
   return sum;
 };
 
-const total = lines.reduce((acc, line) => acc + processLine(line), 0);
+const total = processInput(input);
 console.log(total);
