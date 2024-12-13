d37c4de5eba9e9aad8bde790970cb3cbb3ff0a8e
Will Ness <willness210@gmail.com>
2024-12-12 19:20:34 -0700

    day 10 part 2

--- 2024/src/10/code.ts
+++ 2024/src/10/code.ts
@@ -22,16 +22,17 @@ function exploreUntilPeak(
   grid: number[][],
   position: Position,
   peaksVisited: boolean[][]
-) {
+): number {
   const { x, y } = position;
   const currentValue = grid[y][x];
   if (currentValue === 9) {
     peaksVisited[y][x] = true;
-    return;
+    return 1;
   }
   const width = grid[0].length;
   const height = grid.length;
   const nextValue = currentValue + 1;
+  let score = 0;
   for (const delta of deltas) {
     const newX = x + delta.x;
     const newY = y + delta.y;
@@ -43,12 +44,14 @@ function exploreUntilPeak(
     }
     const nextPosition = { x: newX, y: newY };
 
-    exploreUntilPeak(grid, nextPosition, peaksVisited);
+    score += exploreUntilPeak(grid, nextPosition, peaksVisited);
   }
+  return score;
 }
 
 function getScore(grid: number[][]) {
   let score = 0;
+  let totalPaths = 0;
   for (let y = 0; y < grid.length; y++) {
     for (let x = 0; x < grid[y].length; x++) {
       if (grid[y][x] !== 0) {
@@ -56,14 +59,16 @@ function getScore(grid: number[][]) {
       }
       const position = { x, y };
       const peaksVisited: boolean[][] = grid.map((row) => row.map(() => false));
-      exploreUntilPeak(grid, position, peaksVisited);
+      totalPaths += exploreUntilPeak(grid, position, peaksVisited);
       score += peaksVisited.reduce(
         (acc, row) => acc + row.filter((cell) => cell).length,
         0
       );
     }
   }
-  return score;
+  return { score, totalPaths };
 }
 
-console.log(`The score is: ${getScore(inputAsNumericGrid)}`);
+const { score, totalPaths } = getScore(inputAsNumericGrid);
+console.log(`The score is: ${score}`);
+console.log(`The total number of paths is: ${totalPaths}`);
