3d41673f52b9ab3789ffc6c558e7352c11a9650f
Will Ness <willness210@gmail.com>
2024-12-12 19:47:03 -0700

    day 11 part 2

--- 2024/src/11/code.ts
+++ 2024/src/11/code.ts
@@ -5,32 +5,53 @@ const filename = args[0];
 
 const input = fs.readFileSync(filename, "utf-8");
 
-function stoneTick(num: number): number[] {
-  if (num === 0) {
+function stoneTick(stoneValue: number): number[] {
+  if (stoneValue === 0) {
     return [1];
   }
-  const numStr = num.toString();
+  const numStr = stoneValue.toString();
   if (numStr.length % 2 === 0) {
     const firstHalf = parseInt(numStr.slice(0, numStr.length / 2));
     const secondHalf = parseInt(numStr.slice(numStr.length / 2));
     return [firstHalf, secondHalf];
   }
-  return [num * 2024];
+  return [stoneValue * 2024];
 }
 
 const initialArrangement = input.split(" ").map((num) => parseInt(num));
 
-function getNumberOfStonesAfterBlinks(input: number[], blinks: number): number {
-  let currentArrangement = input;
-  for (let i = 0; i < blinks; i++) {
-    const newArrangement = [];
-    for (const num of currentArrangement) {
-      newArrangement.push(...stoneTick(num));
-    }
-    currentArrangement = newArrangement;
+const memoization: Record<string, number> = {};
+
+function stoneCountAfterTicks(stoneValue: number, ticksLeft: number): number {
+  if (ticksLeft === 0) {
+    return 1;
   }
-  return currentArrangement.length;
+  const newStoneValues = stoneTick(stoneValue);
+  let total = 0;
+  for (const stoneValue of newStoneValues) {
+    const memoizationString = `${stoneValue}-${ticksLeft - 1}`;
+    if (memoization[memoizationString]) {
+      total += memoization[memoizationString];
+      continue;
+    }
+    const stoneValueCount = stoneCountAfterTicks(stoneValue, ticksLeft - 1);
+    memoization[memoizationString] = stoneValueCount;
+    total += stoneValueCount;
+  }
+  return total;
 }
 
-const numberOfStones = getNumberOfStonesAfterBlinks(initialArrangement, 25);
-console.log(`Number of stones after 25 blinks: ${numberOfStones}`);
+function stoneCountAfterTicksForArrangement(
+  arrangement: number[],
+  ticksLeft: number
+): number {
+  return arrangement.reduce(
+    (acc, val) => acc + stoneCountAfterTicks(val, ticksLeft),
+    0
+  );
+}
+
+const result25 = stoneCountAfterTicksForArrangement(initialArrangement, 25);
+console.log(result25);
+const result75 = stoneCountAfterTicksForArrangement(initialArrangement, 75);
+console.log(result75);
