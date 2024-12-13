d6ee72e070581958a80701d7517ee28cb8a434e9
Will Ness <willness210@gmail.com>
2024-12-12 17:50:47 -0700

    day 9 part 2

--- 2024/src/09/code.ts
+++ 2024/src/09/code.ts
@@ -44,6 +44,52 @@ function shiftSystemArray(prevSystemArray: ("." | number)[]): SystemArray {
   return systemArray;
 }
 
+function shiftSystemArray2(prevSystemArray: ("." | number)[]): SystemArray {
+  const systemArray: SystemArray = [...prevSystemArray];
+  function tryToShiftId(id: number, startingIndex: number, length: number) {
+    let startingFreeSpace = -1;
+    for (let i = 0; i < startingIndex; i++) {
+      const currentValue = systemArray[i];
+      if (currentValue !== ".") {
+        startingFreeSpace = -1;
+        continue;
+      }
+      if (startingFreeSpace === -1) {
+        startingFreeSpace = i;
+      }
+      const streakOfFreeSpaces = i - startingFreeSpace + 1;
+      if (streakOfFreeSpaces >= length) {
+        for (let j = 0; j < length; j++) {
+          systemArray[startingFreeSpace + j] = id;
+          systemArray[startingIndex + j] = ".";
+        }
+        return;
+      }
+    }
+  }
+  const alreadyShiftedIds = new Set<number>();
+  for (let i = systemArray.length - 1; i > 0; i--) {
+    const currentValue = systemArray[i];
+    if (systemArray[i - 1] !== currentValue && currentValue !== ".") {
+      const startingIndex = i;
+      let length = 1;
+      for (let j = i + 1; j < systemArray.length; j++) {
+        if (systemArray[j] === currentValue) {
+          length++;
+        } else {
+          break;
+        }
+      }
+      if (alreadyShiftedIds.has(currentValue)) {
+        continue;
+      }
+      tryToShiftId(currentValue, startingIndex, length);
+      alreadyShiftedIds.add(currentValue);
+    }
+  }
+  return systemArray;
+}
+
 function computeSystemArrayChecksum(systemArray: SystemArray): number {
   let checksum = 0;
   for (let i = 0; i < systemArray.length; i++) {
@@ -66,3 +112,15 @@ function getAnswerToPart1(input: string): number {
 }
 
 console.log(getAnswerToPart1(input));
+
+function getAnswerToPart2(input: string): number {
+  const numberArray = convertInputToNumberArray(input);
+
+  const systemArray = getSystemArray(numberArray, 10);
+
+  const shiftedSystemArray = shiftSystemArray2(systemArray);
+
+  return computeSystemArrayChecksum(shiftedSystemArray);
+}
+
+console.log(getAnswerToPart2(input));
